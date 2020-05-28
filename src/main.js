33eb6d4f6598135612d2022a940f60266cc93152
TheQuail13 <dougthierjung@gmail.com>
2020-05-27 22:20:43 -0700

    adding google map plus config

--- src/main.js
+++ src/main.js
@@ -4,9 +4,17 @@ import './registerServiceWorker'
 import router from './router'
 import './quasar'
 import axios from 'axios'
+import * as GmapVue from 'gmap-vue'
 
 Vue.prototype.$http = axios
 Vue.config.productionTip = false
+
+Vue.use(GmapVue, {
+  load: {
+    key: process.env.GOOGLE_API_KEY,
+    installComponents: true
+  }
+})
 
 new Vue({
   router,
